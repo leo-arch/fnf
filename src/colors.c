8934faddb0902e7dfc82425f75bcf416f205d1f1
leo-arch <leonardoabramovich2@gmail.com>
2025-08-01 16:27:49 -0300

    Replace strncpy by memcpy

--- src/colors.c
+++ src/colors.c
@@ -243,7 +243,7 @@ set_colors(tty_interface_t *state)
 	char def_colors[sizeof(DEFAULT_COLORS)];
 	env = getenv("FNF_COLORS");
 	if (!env || !*env) {
-		strncpy(def_colors, DEFAULT_COLORS, sizeof(def_colors));
+		memcpy(def_colors, DEFAULT_COLORS, sizeof(def_colors));
 		env = def_colors;
 	}
 
